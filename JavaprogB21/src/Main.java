ad0119da3c3a5e397d2db4aeef0839dfe1e4440a
Turcan1 <74077319+Turcan1@users.noreply.github.com>
2020-11-10 14:53:00 -0800

    update

--- JavaprogB21/src/Main.java
+++ JavaprogB21/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Good buy World");
-        System.out.println("Good buy World");
+        System.out.println("Good buy ");
         //Mac users ar not that happy
         //all users sometimes are happy
     }
